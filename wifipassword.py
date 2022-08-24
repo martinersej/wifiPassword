072e8c7e1f4e144d4522cd5cb5d21d0d2b1ae2bb
Martin <77696248+martinersej@users.noreply.github.com>
2022-08-24 14:52:57 +0200

    Update wifipassword.py

--- wifipassword.py
+++ wifipassword.py
@@ -1,4 +1,3 @@
-
 #! py
 #################################################################################
 # Copyright of David Bombal, 2021                                               #
@@ -16,14 +15,22 @@ from sys import platform
 from unittest import skip
 
 if __name__ == '__main__':
-    print(platform)
     if platform == "linux" or platform == "linux2":
         skip
     elif platform == "darwin":
         command_output = subprocess.run(["networksetup", "-listpreferredwirelessnetworks", "en0"], capture_output=True, stdin=subprocess.DEVNULL).stdout.decode()
-        print(command_output)
-        #profile_names = (re.findall("All User Profile     : (.*)\r", command_output))
-        skip
+        profile_names = (re.findall("\t(.*)", command_output))
+        if len(profile_names) != 0:
+            listWifi = []
+            for name in profile_names:
+                wifiProfile = {}
+                profileInfoPasswd = subprocess.run(["security", "find-generic-password", "-D", "'AirPort network password'", "-ga", '\"{}\"'.format(name)], capture_output = True, stdin=subprocess.DEVNULL).stdout.decode()
+                print(profileInfoPasswd)
+                if re.search("password: (.*)\r", profileInfoPasswd) == None:
+                    wifiProfile["ssid"] = name
+                    password = re.search("password: (.*)\r", profileInfoPasswd)
+                    wifiProfile["password"] = password if password != None else None
+                    listWifi.append(wifiProfile)
     elif platform == "win32":
         command_output = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output=True, stdin=subprocess.DEVNULL).stdout.decode()
         profile_names = (re.findall("All User Profile     : (.*)\r", command_output))
@@ -38,6 +45,6 @@ if __name__ == '__main__':
                     password = re.search("Key Content            : (.*)\r", profileInfoPasswd)
                     wifiProfile["password"] = password[1] if password != None else None
                     listWifi.append(wifiProfile)
-            listWifi.sort(key=lambda x: x['ssid'])
-        for x in range(len(listWifi)):
-            print('Name: {}, Password: {}'.format(listWifi[x]['ssid'], listWifi[x]['password']))
+    listWifi.sort(key=lambda x: x['ssid'])
+    for x in range(len(listWifi)):
+        print('Name: {}, Password: {}'.format(listWifi[x]['ssid'], listWifi[x]['password']))
